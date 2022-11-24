3037379b030821eda7ab44291135247eddc56e7c
jszufa <jerzy.szufa@gmail.com>
2022-11-24 13:07:04 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+firstNumber = 3
+secondNumber = 4
+
+function add(num1, num2){
+    let result = num1 + num2;
+    return result;
+}
+
+/* sprawdzenie */
+console.log(add(firstNumber, secondNumber))
